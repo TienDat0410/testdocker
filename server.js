27becbc9aefb589d648d0c150f4a7a1470c70554
phạm huỳnh tiến đạt <82532351+TD-Front-end@users.noreply.github.com>
2022-10-22 23:02:44 +0700

    toi da hoan thanh cv 1

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/' , (req , res)=>{
 
    res.send('<h2>Docker hello sờ lô </h2>)');
 });
+//cong viec 1
+app.get('/cv1' , (req , res)=>{
+
+    res.send('<h2>Đây la công việc 1 </h2>)');
+ });
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
